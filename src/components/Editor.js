2ca1ba6f9cfeae65c988552c09d7ccad98145ab4
Matthias Wilhalm <matthias.wilhalm@gmail.com>
2022-01-03 15:25:49 +0100

    init w/ quiz question editor

--- src/components/Editor.js
+++ src/components/Editor.js
@@ -8,7 +8,8 @@ const Editor = props => {
     const [, forceUpdate] = useReducer(x => x + 1, 0);
 
     const [event, setEvent] = useState(new Event("", []));
-    const [newGameSelector, setNewGameSelector] = useState("quiz")
+    const [newGameSelector, setNewGameSelector] = useState("quiz");
+    const [focusedGame, setFocusedGame] = useState(0);
 
     const handleUpdate = (name, value) => {
         event[name] = value;
@@ -27,6 +28,11 @@ const Editor = props => {
         handleUpdate('games', event.games);
     }
 
+    const updateQuestion = (name, value, questionIndex, game) => {
+        game.content.questions[questionIndex][name] = value;
+        handleUpdate('games', event.games);
+    }
+
     const createNewGame = () => {
         let g = event.games;
         g.push(new Game('', '', newGameSelector, newGameSelector==='bingo', {
@@ -39,68 +45,181 @@ const Editor = props => {
         handleUpdate('games', g);
     }
 
-    const renderGame = (game, index) => {
+    const addNewContentUnit = game => {
         switch (game.type) {
             case 'quiz':
-                return(
-                    <li>
-                        <input 
-                            type={"text"}
-                            value={game.title}
-                            placeholder="Title"
-                            onChange={e => updateGame("title", e.target.value, game)}
-                            className="double"
-                        ></input>
-                        <textarea
-                            value={game.description}
-                            placeholder="description"
-                            onChange={e => updateGame("description", e.target.value, game)}
-                            className="double"
-                        ></textarea>
-                        <input 
-                            type={"number"}
-                            value={game.content?.scoreWin}
-                            placeholder="Score Win"
-                            onChange={e => updateContent("scoreWin", e.target.value, game)}
-                            className="double"
-                        ></input>
-                        <input 
-                            type={"number"}
-                            value={game.content?.scoreSpecWin}
-                            placeholder="Score Spec Win"
-                            onChange={e => updateContent("scoreSpecWin", e.target.value, game)}
-                            className="double"
-                        ></input>
-                        <input 
-                            type={"number"}
-                            value={game.content?.scoreLose}
-                            placeholder="Score Lose"
-                            onChange={e => updateContent("scoreLose", e.target.value, game)}
-                            className="double"
-                        ></input>
-                        <input 
-                            type={"number"}
-                            value={game.content?.scoreSpecLose}
-                            placeholder="Score Spec Lose"
-                            onChange={e => updateContent("scoreSpecLose", e.target.value, game)}
-                            className="double"
-                        ></input>
-                    </li>
-                );                
-            case 'queue':
-                return(
-                    <li>
-
-                    </li>
-                );
-            case 'bingo':
-                return (
-                    <li>
-
-                    </li>
-                );
+                if(game?.content?.questions instanceof Array) {
+                    game.content.questions.push({
+                        question: '',
+                        url: '',
+                        urlType: "none",
+                        answerType: "preset",
+                        randomize: false,
+                        presetAnswers: []
+                    });
+                    updateContent('questions', game.content.questions, game);
+                }
+                break;
+        
             default:
-                return null;
+                break;
+        }
+    }
+
+    const addAnswerForQuizUnit = (game, questionIndex) => {
+        if(game?.content?.questions instanceof Array) {
+            game.content.questions[questionIndex].presetAnswers.push({
+                correct: false,
+                text: "",
+                url: "",
+                urlType: "none"
+            }); 
+            updateContent('questions', game.content.questions, game);
+        }
+    }
+
+    const renderGame = (game, index) => {
+        if(focusedGame === index) {
+            switch (game.type) {
+                case 'quiz':
+                    return(
+                        <li>
+                            <input 
+                                type={"text"}
+                                value={game.title}
+                                placeholder="Title"
+                                onChange={e => updateGame("title", e.target.value, game)}
+                                className="double"
+                            ></input>
+                            <textarea
+                                value={game.description}
+                                placeholder="description"
+                                onChange={e => updateGame("description", e.target.value, game)}
+                                className="double"
+                            ></textarea>
+                            <input 
+                                type={"number"}
+                                value={game.content?.scoreWin}
+                                placeholder="Score Win"
+                                onChange={e => updateContent("scoreWin", e.target.value, game)}
+                                className="double"
+                            ></input>
+                            <input 
+                                type={"number"}
+                                value={game.content?.scoreSpecWin}
+                                placeholder="Score Spec Win"
+                                onChange={e => updateContent("scoreSpecWin", e.target.value, game)}
+                                className="double"
+                            ></input>
+                            <input 
+                                type={"number"}
+                                value={game.content?.scoreLose}
+                                placeholder="Score Lose"
+                                onChange={e => updateContent("scoreLose", e.target.value, game)}
+                                className="double"
+                            ></input>
+                            <input 
+                                type={"number"}
+                                value={game.content?.scoreSpecLose}
+                                placeholder="Score Spec Lose"
+                                onChange={e => updateContent("scoreSpecLose", e.target.value, game)}
+                                className="double"
+                            ></input>
+                        </li>
+                    );                
+                case 'queue':
+                    return(
+                        <li>
+    
+                        </li>
+                    );
+                case 'bingo':
+                    return (
+                        <li>
+    
+                        </li>
+                    );
+                default:
+                    return null;
+            }
+        } else {
+            return (
+                <li>
+                    <h3>{game.title!=='' ? game.title : game.type}</h3>
+                    <button className="small-button" onClick={() => setFocusedGame(index)}>up</button>
+                </li>
+            );
+        }
+    }
+
+    const renderQuizAnswers = question => {
+        return (
+            <ul className="editor-main-inner-list triple">
+                {question.presetAnswers.map(a =>
+                    <li>
+                        <hr className="triple"></hr>
+                        <input className="double" value={a.text} placeholder="Answer"></input>
+                        <div>
+                            <input type="checkbox" checked={a.correct}></input>
+                            <label>correct</label>
+                        </div>
+                        <input className="double" value={a.url} placeholder="URL"></input>
+                        <select value={a.urlType}>
+                            <option value={'none'}>none</option>
+                            <option value={'image'}>image</option>
+                            <option value={'video'}>video</option>
+                            <option value={'audio'}>audio</option>
+                            <option value={'iframe'}>iframe</option>
+                        </select>
+                    </li>
+                )}
+            </ul>
+        );
+    }
+
+    const renderGameEditList = game => {
+        switch (game.type) {
+            case 'quiz':
+                return (
+                    <ul className="editor-main-list">
+                        <li>
+                            <div></div>
+                            <div></div>
+                            <button className="normal-button editor-add-button" onClick={() => addNewContentUnit(game)}>Add</button>
+                        </li>
+                        {game.content.questions.map((a, i) => 
+                            <li>
+                                <input 
+                                    className="double"
+                                    value={a.question}
+                                    onChange={e => updateQuestion('question', e.target.value, i, game)}
+                                    placeholder="question">
+                                </input>
+                                <div>
+                                    <button className="small-button">-</button>
+                                    <button className="small-button" onClick={() => addAnswerForQuizUnit(game, i)}>+</button>
+                                </div>
+                                <input 
+                                    className="double"
+                                    value={a.url}
+                                    placeholder="URL"
+                                    onChange={e => updateQuestion('url', e.target.value, i, game)}>
+                                </input>
+                                <select value={a.urlType} onChange={e => updateQuestion('urlType', e.target.value,i ,game)}>
+                                    <option value={'none'}>none</option>
+                                    <option value={'image'}>image</option>
+                                    <option value={'video'}>video</option>
+                                    <option value={'audio'}>audio</option>
+                                    <option value={'iframe'}>iframe</option>
+                                </select>
+                                {renderQuizAnswers(a)}
+                            </li>
+                        )}
+                    </ul>
+                );
+        
+            default:
+                break;
         }
     }
 
@@ -145,9 +264,7 @@ const Editor = props => {
                 </ul>
             </div>
             <div className="panel">
-                <ul>
-
-                </ul>
+                {renderGameEditList(event.games?.[focusedGame] ?? [])}
             </div>
         </div>
     );
